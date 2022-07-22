31b71cade4af390c67eb086e0087242c98fb68e2
vi1110111 <itsenkovlad.work@gmail.com>
2022-07-22 20:39:12 +0300

    Lesson 138-139

--- Section 10 (Functions)/script.js
+++ Section 10 (Functions)/script.js
@@ -1,8 +1,56 @@
 "use strict";
+// //
+// //
+// // 139 Codding challange 2
+// (function () {
+//   const header = document.querySelector("h1");
+//   header.style.color = "red";
+
+//   document.querySelector("body").addEventListener("click", function () {
+//     header.style.color = "blue";
+//   });
+// })();
+
 //
 //
 // 138 More Closures
+// ex 1
+// let f;
 
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+
+// h();
+// f();
+
+// // ex 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+// boardPassengers(180, 3);
+//
+//
 // 137 Closures
 // const secureBooking = function () {
 //   let passengerCount = 0;
