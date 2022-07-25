abc9e19bd28ab7dee0166788a1fe3b98352bb012
vi1110111 <itsenkovlad.work@gmail.com>
2022-07-25 22:54:25 +0300

    Lesson 187-190

--- Section 13 (Advanced-DOM)/script.js
+++ Section 13 (Advanced-DOM)/script.js
@@ -37,7 +37,8 @@ message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got IT</button>';
 
 const header = document.querySelector('.header');
-header.append(message);
+//header.append(message);
+header.before(message);
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
@@ -48,7 +49,18 @@ message.style.height =
   Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
 
 message.style.backgroundColor = '#37383d';
-message.style.width = '100';
+message.style.width = '100%';
+
+///////////////////////////////////////////////// 188 Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  const section1 = document.querySelector('#section--1');
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////////////// 191 Event Propagation in Practice
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -121,3 +133,54 @@ message.style.width = '100';
 // logo.classList.remove('c');
 // logo.classList.toggle('c');
 // logo.classList.contains('c');
+
+// //
+// //
+// // 188 Implementing Smooth Scrolling
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+
+//   console.log('current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+//   // Scroll
+//   // OLD WAY SIMPLE ONE
+//   // window.scrollTo(
+//   //   s1coords.left + window.pageXOffset,
+//   //   s1coords.top + window.pageYOffset
+//   // );
+//   // OLD WAY SMOOTH ONE
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset,
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth',
+//   // });
+//   // MODERN WAY
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+// //
+// //
+// // 188 Types of Events and Event Handlers
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   console.log('addEventListener: You are reading the heading note!');
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// // Remove EventListener
+// setTimeout(() => {
+//   h1.removeEventListener('mouseenter', alertH1);
+// }, 3000);
+
+// // Exist also event property
+// //h1.onmouseenter = alertH1;
+
+//
+//
+// 191 Event Propagation in Practice
