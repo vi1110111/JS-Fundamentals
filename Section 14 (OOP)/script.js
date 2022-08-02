10f53e88efd0ab8587d2fd25ca1217a419f1ecf0
vi1110111 <itsenkovlad.work@gmail.com>
2022-08-02 23:48:50 +0300

    Lesson 208-211

--- Section 14 (OOP)/script.js
+++ Section 14 (OOP)/script.js
@@ -1,23 +1,55 @@
 'use strict';
 
-// //
-// //
-// // 208 Constructor Functions and the new Operator
+//
+//
+// 208 Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-// const Person = function (firstName, birthYear) {
-//   this.firstName = firstName;
-//   this.birthYear = birthYear;
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
 
-//   // Never do this
-//   this.calcAge = function () {
-//     console.log(2037 - this.birthYear);
-//   };
-// };
+const vi = new Person('VI', 2001);
+// Steps when function is called
+// 1. New empty object is created {}
+// 2. function is called. this = {} new empty object
+// 3. {} linked to prototupe
+// 4. function automatically return {}
+console.log(vi);
 
-// const vi = new Person('VI', 2001);
-// // Steps when function is called
-// // 1. New empty object is created {}
-// // 2. function is called. this = {} new empty object
-// // 3. {} linked to prototupe
-// // 4. function automatically return {}
-// console.log(vi);
+//
+//
+// 209 Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+vi.calcAge();
+// !!!
+console.log(vi.__proto__ === Person.prototype);
+
+Person.prototype.species = 'Homo';
+console.log(vi.species);
+
+console.log(vi.hasOwnProperty('firstName'));
+
+//
+//
+// 211 Prototypal Inheritance on Built-In Objects
+console.log(vi.__proto__);
+console.log(vi.__proto__.__proto__);
+console.log(vi.__proto__.__proto__.__proto__);
+
+const arr = [5, 7, 3, 7, 8, 1];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
